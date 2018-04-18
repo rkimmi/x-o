70b7d69c9488ead624fc039a7ceed95828f034a0
rkimmi <kimmirindel97@gmail.com>
2018-04-18 13:09:58 +1200

    set up board

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
 
-const App = () => (
-  <div className='app-container'>
-    <p>Hiya, good luck!</p>
-    <iframe src="https://giphy.com/embed/yoJC2AkZpy7PffDToY" width="480" height="359" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
-  </div>
-)
+class App extends React.Component {
+  constructor (props) {
+    super (props)
+    this.state = {
+        playerOutput: null
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick () {
+
+  }
+
+  render () {
+    return (
+      <div className='board'>
+        <div className='row'>
+          <div className='top-left spot'>o</div>
+          <div className='top-middle spot'>o</div>
+          <div className='top-right spot'>o</div>
+        </div>
+
+        <div className='row'>
+          <div className='middle-left spot' onClick={this.handleClick}>{this.state.playerOutput}</div>
+          <div className='middle spot'>o</div>
+          <div className='middle-right spot'>o</div>
+        </div>
+
+        <div className='row'>
+          <div className='bottom-left spot'>o</div>
+          <div className='bottm-middle spot'>o</div>
+          <div className='bottom-right spot'>o</div>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default App
--- public/bundle.js
+++ public/bundle.js
@@ -18275,24 +18275,107 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _react = __webpack_require__(2);
 
 var _react2 = _interopRequireDefault(_react);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var App = function App() {
-  return _react2.default.createElement(
-    'div',
-    { className: 'app-container' },
-    _react2.default.createElement(
-      'p',
-      null,
-      'Hiya, good luck!'
-    ),
-    _react2.default.createElement('iframe', { src: 'https://giphy.com/embed/yoJC2AkZpy7PffDToY', width: '480', height: '359', frameBorder: '0', className: 'giphy-embed', allowFullScreen: true })
-  );
-};
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var App = function (_React$Component) {
+  _inherits(App, _React$Component);
+
+  function App(props) {
+    _classCallCheck(this, App);
+
+    var _this = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this, props));
+
+    _this.state = {
+      playerOutput: null
+    };
+    _this.handleClick = _this.handleClick.bind(_this);
+    return _this;
+  }
+
+  _createClass(App, [{
+    key: 'handleClick',
+    value: function handleClick() {}
+  }, {
+    key: 'render',
+    value: function render() {
+      return _react2.default.createElement(
+        'div',
+        { className: 'board' },
+        _react2.default.createElement(
+          'div',
+          { className: 'row' },
+          _react2.default.createElement(
+            'div',
+            { className: 'top-left spot' },
+            'o'
+          ),
+          _react2.default.createElement(
+            'div',
+            { className: 'top-middle spot' },
+            'o'
+          ),
+          _react2.default.createElement(
+            'div',
+            { className: 'top-right spot' },
+            'o'
+          )
+        ),
+        _react2.default.createElement(
+          'div',
+          { className: 'row' },
+          _react2.default.createElement(
+            'div',
+            { className: 'middle-left spot', onClick: this.handleClick },
+            this.state.playerOutput
+          ),
+          _react2.default.createElement(
+            'div',
+            { className: 'middle spot' },
+            'o'
+          ),
+          _react2.default.createElement(
+            'div',
+            { className: 'middle-right spot' },
+            'o'
+          )
+        ),
+        _react2.default.createElement(
+          'div',
+          { className: 'row' },
+          _react2.default.createElement(
+            'div',
+            { className: 'bottom-left spot' },
+            'o'
+          ),
+          _react2.default.createElement(
+            'div',
+            { className: 'bottm-middle spot' },
+            'o'
+          ),
+          _react2.default.createElement(
+            'div',
+            { className: 'bottom-right spot' },
+            'o'
+          )
+        )
+      );
+    }
+  }]);
+
+  return App;
+}(_react2.default.Component);
 
 exports.default = App;
 
